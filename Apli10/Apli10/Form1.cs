7219e87218db8b1c1cb98b0d8114aa71afd0cada
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-15 16:20:08 -0400

    apli10

---
+++ Apli10/Apli10/Form1.cs
@@ -0,0 +1,15 @@
+namespace Apli10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola,soy Luis osvaldo y MT-2025-00801"); 
+        }
+    }
+}
